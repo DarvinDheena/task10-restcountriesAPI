135b9bceafd6b41cb8b5f0c3a80bf6ae311caff5
DarvinDheena <131634107+DarvinDheena@users.noreply.github.com>
2023-09-26 16:17:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const apiKey = "https://restcountries.com/v3.1/name/";
+let showDetails = document.getElementById('countryDetails');
+
+async function getCountryDetails (countryName){
+    try{
+        let respose = await fetch(`https://restcountries.com/v3.1/name/${countryName}`)
+        let data = await respose.json();
+        console.log(data);
+        let commonName = data[1].name.common ;
+        console.log(commonName);
+        let countryCapital = data[1].capital[0] ;
+        let area = data[1].area;
+        let population = data[1].population;
+        let lattitude = data[1].latlng[0];
+        let longitude = data[1].latlng[1];
+
+        let results = 
+        `<div>
+            <h3 style="text-align:center;"> Entered Country : ${commonName}  </h5><br>
+            <h5>Capital of the Country : ${countryCapital}</h5>
+            <h5>Area of the Country : ${area} </h5>
+            <h5>population of the Country : ${population}</h5>
+            <h5>Lattitude : ${lattitude}</h5>
+            <h5>longitude : ${longitude}</h5>
+        </div>`
+        showDetails.innerHTML= results ;
+    }
+    catch(error) {
+        console.error("Error Fetching Data  :",error );
+    }
+}
+let button = document.getElementById('button');
+button.addEventListener('click',(e)=>{
+    e.preventDefault();
+    let countryName = document.getElementById('countryName').value;
+    getCountryDetails(countryName);
+})
